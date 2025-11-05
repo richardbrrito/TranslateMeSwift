d060fb497c07fd7443239621c953bfc81e7863d8
Richard Brito <richardbrito116@gmail.com>
2025-11-04 19:04:17 -0500

    Initial Commit

---
+++ FireChat/FireChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireChatApp.swift
+//  FireChat
+//
+//  Created by Richard Brito on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FireChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FireChatTests/FireChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  FireChatTests.swift
+//  FireChatTests
+//
+//  Created by Richard Brito on 11/4/25.
+//
+
+import Testing
+@testable import FireChat
+
+struct FireChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
